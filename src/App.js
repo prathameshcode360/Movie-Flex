989ad43ef93bcba68db5280b3086f9724c37097b
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-06-03 11:41:01 +0530

    back to main project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import MovieCard from "./movieCard";
-import Student from "./student";
 
 function App() {
   return (
     <>
-      {/* <h1>Movie-Flex</h1>; */}
-      {/* <MovieCard /> */}
-      <Student name="Alexa" />
-      <Student />
+      <h1>Movie-Flex</h1>;
+      <MovieCard />
     </>
   );
 }
-
-Student.defaultProps = {
-  name: "Student",
-};
 
 export default App;
