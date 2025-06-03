d68d2c241677a27e9a08aca0a35b3f704d01e273
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-06-03 11:39:41 +0530

    Practice with props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import MovieCard from "./movieCard";
+import Student from "./student";
 
 function App() {
   return (
     <>
-      <h1>Movie-Flex</h1>;
-      <MovieCard />
+      {/* <h1>Movie-Flex</h1>; */}
+      {/* <MovieCard /> */}
+      <Student name="Alexa" />
+      <Student />
     </>
   );
 }
+
+Student.defaultProps = {
+  name: "Student",
+};
 
 export default App;
